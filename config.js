bcf22e6c10513225281cd33f9bfbd5c615c5ba40
bigblackfootman <68459060+bigblackfootman@users.noreply.github.com>
2022-04-22 01:41:08 -0700

    Minor changes

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
     app: {
         px: 'm!',
         token: 'ODk5MzEwMDYyMjYxMTI1MTcw.YWw5ww.1SjB9Myi9gzMn5siRrC_DqLiHzY',
-        playing: 'Music!'
+        playing: 'Music at Pyreworks!'
     },
 
     opt: {
         DJ: {
-            enabled: false,
+            enabled: true,
             roleName: 'DJ',
-            commands: ['back', 'filter', 'pause', 'resume', 'seek', 'skip']
+            commands: ['back', 'filter', 'pause', 'resume', 'seek', 'forceskip']
         },
         maxVol: 100,
         loopMessage: false,
