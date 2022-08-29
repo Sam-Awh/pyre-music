f5fcf1a8c331236d8547fa3d23545306a4c3dfd8
bigblackfootman <dotblank69@gmail.com>
2022-08-29 23:03:56 +0530

    env security patch

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { Player } = require('discord-player');
 const { Client, Intents } = require('discord.js');
+require("dotenv").config();
 
 global.client = new Client({
     intents: [
@@ -18,4 +19,4 @@ global.player = new Player(client, client.config.opt.discordPlayer);
 require('./src/loader');
 require('./src/events');
 
-client.login(client.config.app.token);
+client.login(process.env.TOKEN);
